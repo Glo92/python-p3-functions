e65786d89cc861e79057f369bbfb7a1d9f530293
Gloria Wekura <wekuraglorian07@gmail.com>
2024-05-27 18:40:52 +0300

    submit lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,33 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
     pass
+    sum = num1 + num2
+    print(sum)
+    return sum
+
+add(45, 55)
 
 def halve(number):
     pass
+    half = number / 2
+    return half
+ 
+       
